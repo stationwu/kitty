68b46621a0e1a6858f376e11dd4db078208eef51
Arthur Wu <stationwu@163.com>
2018-02-26 10:48:51 +0800

    initial

--- kitty/pages/index/index.js
+++ kitty/pages/index/index.js
@@ -56,23 +56,30 @@ Page({
   onLoad: function () {
     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
     // 所以此处加入 callback 以防止这种情况
-    var that = this;
-    wx.getUserInfo({
-      success: res => {
-        app.globalData.userInfo = res.userInfo
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-        wx.login({
-          success: function (res) {
-            var code = res.code;
-            // --------- 发送凭证 ------------------
-            loginBackend(that, code);
-          }
-        });
-      }
-    })
+    if (app.globalData.userInfo && wx.getStorageSync('openid') !== "" ) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }else{
+      var that = this;
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+          wx.login({
+            success: function (res) {
+              var code = res.code;
+              // --------- 发送凭证 ------------------
+              loginBackend(that, code);
+            }
+          });
+        }
+     })
+    }
   },
   onShow:function(e){
     var that = this;
